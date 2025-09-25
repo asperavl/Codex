57860ee08d3c16b59465dc3834c0ff77adac4913
asperavl <140082123+asperavl@users.noreply.github.com>
2025-09-25 23:38:29 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import dynamic from "next/dynamic";
+const EditorComponent = dynamic(() => import("@/components/EditorComponent"), { ssr: false });
+import { ModeToggleBtn } from "@/components/mode-toggle-btn";
+import React from "react";
+
+
+export default function Home() {
+
+
+
+  return (
+    <div className="dark:bg-slate-800 bg-slate-300 p-8">
+      <EditorComponent/>
+    </div>
+  );
+}
